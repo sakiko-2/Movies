aa0f7391896e3a9ae8cf081553e0ebbe5fa63045
sakiko <sakiko.ue@gmail.com>
2017-10-28 22:27:24 +1300

    align image and text

--- App.js
+++ App.js
@@ -17,12 +17,14 @@ export default class App extends React.Component {
     var movie = MOCKED_MOVIES_DATA[0];
     return (
       <View style={styles.container}>
-        <Text>{movie.title}</Text>
-        <Text>{movie.year}</Text>
         <Image
           source={{uri: movie.posters.thumbnail}}
           style={styles.thumbnail}
         />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
       </View>
     );
   }
@@ -31,9 +33,21 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  rightContainer: {
+    flex: 1
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
   },
   thumbnail: {
     width: 53,
